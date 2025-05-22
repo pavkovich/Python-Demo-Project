145891d098bf57714e1909808d0231afc14aae81
Robert Pavkovich <rpavkovich@gmail.com>
2025-05-22 11:45:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get("https://www.example.com", timeout=None)
+response = requests.get("https://www.example.com", timeout=5)
 if response.status_code == 200:
     print("Successfully reached example.com")
 else:
